5d4b7e5bc09bb06f3b4c6dd3ed6707e0f50f182b
SondreHal <sondrehal.kodehode@gmail.com>
2022-08-01 14:10:54 +0200

    hoi

---
+++ index.js
@@ -0,0 +1,875 @@
+let bosses = [
+	{
+		name: "Zakum",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 1000000,
+		mesosChanged: 1000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Zakum",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 3062500,
+		mesosChanged: 3062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Zakum",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 81000000,
+		mesosChanged: 81000000,
+		mesosUniqueAdd: 0.4,
+		crystals: [],
+	},
+	{
+		name: "Magnus",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 3610000,
+		mesosChanged: 3610000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Magnus",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 12960000,
+		mesosChanged: 12960000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Magnus",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 95062500,
+		mesosChanged: 95062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Hilla",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4000000,
+		mesosChanged: 4000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Hilla",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 56250000,
+		mesosChanged: 56250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "OMNI_CLN",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 6250000,
+		mesosChanged: 6250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Papulatus",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 3422500,
+		mesosChanged: 3422500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Papulatus",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 13322500,
+		mesosChanged: 13322500,
+		mesosUniqueAdd: 0.1,
+		crystals: [],
+	},
+	{
+		name: "Papulatus",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 132250000,
+		mesosChanged: 132250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Pierre",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4840000,
+		mesosChanged: 4840000,
+		mesosUniqueAdd: 0.1,
+		crystals: [],
+	},
+	{
+		name: "Pierre",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 81000000,
+		mesosChanged: 81000000,
+		mesosUniqueAdd: 0.1,
+		crystals: [],
+	},
+	{
+		name: "Von Bon",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4840000,
+		mesosChanged: 4840000,
+		mesosUniqueAdd: 0.2,
+		crystals: [],
+	},
+	{
+		name: "Von Bon",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 81000000,
+		mesosChanged: 81000000,
+		mesosUniqueAdd: 0.2,
+		crystals: [],
+	},
+	{
+		name: "Crimson Queen",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4840000,
+		mesosChanged: 4840000,
+		mesosUniqueAdd: 0.3,
+		crystals: [],
+	},
+	{
+		name: "Crimson Queen",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 81000000,
+		mesosChanged: 81000000,
+		mesosUniqueAdd: 0.3,
+		crystals: [],
+	},
+	{
+		name: "Vellum",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4840000,
+		mesosChanged: 4840000,
+		mesosUniqueAdd: 0.4,
+		crystals: [],
+	},
+	{
+		name: "Vellum",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 105062500,
+		mesosChanged: 105062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Von Leon",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 5290000,
+		mesosChanged: 5290000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Von Leon",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 7290000,
+		mesosChanged: 7290000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Von Leon",
+		difficulty: "Hard",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 12250000,
+		mesosChanged: 12250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Horntail",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4410000,
+		mesosChanged: 4410000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Horntail",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 5062500,
+		mesosChanged: 5062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Horntail",
+		difficulty: "Chaos",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 6760000,
+		mesosChanged: 6760000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Arkarium",
+		difficulty: "Easy",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 5760000,
+		mesosChanged: 5760000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Arkarium",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 12602500,
+		mesosChanged: 12602500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Pink Bean",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 7022500,
+		mesosChanged: 7022500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Pink Bean",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 64000000,
+		mesosChanged: 64000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Cygnus",
+		difficulty: "Easy",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 45562500,
+		mesosChanged: 45562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Cygnus",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 72250000,
+		mesosChanged: 72250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Lotus",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 162562500,
+		mesosChanged: 162562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Lotus",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 370562500,
+		mesosChanged: 370562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Damien",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 169000000,
+		mesosChanged: 169000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Damien",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 351562500,
+		mesosChanged: 351562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Guardian Angel Slime",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 171610000,
+		mesosChanged: 171610000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Guardian Angel Slime",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 451562500,
+		mesosChanged: 451562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Lucid",
+		difficulty: "Easy",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 175562500,
+		mesosChanged: 175562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Lucid",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 203062500,
+		mesosChanged: 203062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Lucid",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 400000000,
+		mesosChanged: 400000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Will",
+		difficulty: "Easy",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 191275000,
+		mesosChanged: 191275000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Will",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 232562500,
+		mesosChanged: 232562500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Will",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 441000000,
+		mesosChanged: 441000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Gloom",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 248062500,
+		mesosChanged: 248062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Gloom",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 462250000,
+		mesosChanged: 462250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Verus Hilla",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 447600000,
+		mesosChanged: 447600000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Verus Hilla",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 552250000,
+		mesosChanged: 552250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Darknell",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 264062500,
+		mesosChanged: 264062500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Darknell",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 484000000,
+		mesosChanged: 484000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Black Mage",
+		difficulty: "Hard",
+		type: "Monthly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 2500000000,
+		mesosChanged: 2500000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Chosen Seren",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 668437500,
+		mesosChanged: 668437500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Chosen Seren",
+		difficulty: "Hard",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 756250000,
+		mesosChanged: 756250000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Kalos The Guardian",
+		difficulty: "Chaos",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 1000000000,
+		mesosChanged: 1000000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Ranmaru",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 4202500,
+		mesosChanged: 4202500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Ranmaru",
+		difficulty: "Hard",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 13322500,
+		mesosChanged: 13322500,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Princess No",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 81000000,
+		mesosChanged: 81000000,
+		mesosUniqueAdd: 0.4,
+		crystals: [],
+	},
+	{
+		name: "Julieta",
+		difficulty: "Normal",
+		type: "Daily",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 6000000,
+		mesosChanged: 6000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+	{
+		name: "Akechi Mitsuhide",
+		difficulty: "Normal",
+		type: "Weekly",
+		previousValue: 0,
+		previousValueTwo: 1,
+		mesos: 144000000,
+		mesosChanged: 144000000,
+		mesosUniqueAdd: 0,
+		crystals: [],
+	},
+];
+
+const bossesLs = JSON.parse(localStorage.getItem("info"));
+
+if (bossesLs) {
+	bosses = bossesLs;
+}
+
+const bossesContainer = document.querySelector("#bosses");
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    MAKING EACH BOSS LINE UNIQUE
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+function renderHtml() {
+	for (let i = 0; i < bosses.length; i++) {
+		//displays boss names & difficulty in html
+		bossesContainer.children[i].children[1].children[0].children[1].textContent =
+			bosses[i].difficulty + " " + bosses[i].name;
+
+		//finds images for bosses in html
+		bossesContainer.children[i].children[1].children[0].children[0].src =
+			"images/" + bosses[i].name.replace(/\s+/g, "_").toLocaleLowerCase() + ".png";
+
+		//displays mesos value of boss crystal in html
+		bossesContainer.children[i].children[1].children[1].textContent =
+			bosses[i].type +
+			": " +
+			Math.floor(bosses[i].mesos / bosses[i].previousValueTwo).toLocaleString() +
+			" mesos";
+
+		//gives id to inputs on right side of html
+		bossesContainer.children[i].children[1].children[2].children[0].setAttribute(
+			"id",
+			"characters" + bosses[i].name.replace(/\s+/g, "") + bosses[i].difficulty
+		);
+
+		//gives id to inputs on left side of html
+		bossesContainer.children[i].children[0].setAttribute(
+			"id",
+			"charactersInParty" +
+				bosses[i].name.replace(/\s+/g, "") +
+				bosses[i].difficulty
+		);
+	}
+}
+renderHtml();
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    EVENT LISTENER FOR INPUTS ON LEFT SIDE
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+for (let i = 0; i < bosses.length; i++) {
+	bossesContainer.children[i].children[0].addEventListener("change", (event) => {
+		const buttonId = document.querySelector(`#${event.target.id}`);
+		const buttonValue = parseInt(buttonId.value);
+		const bossCrystals = bosses[i].crystals;
+		const type = bosses[i].type;
+		const bossMesos = bosses[i].mesos;
+		const bossMesosUniqueAdd = bosses[i].mesosUniqueAdd;
+
+		bosses[i].previousValueTwo = parseInt(buttonId.value);
+		//updates base mesos value for crystals in bosses objects array
+		bosses[i].mesosChanged = Math.floor(bossMesos / buttonValue);
+
+		//updates mesos values of current crystals in bosses objects array
+		for (let i = 0; i < bossCrystals.length; i++) {
+			bossCrystals[i] = Math.floor(bossMesos / buttonValue) + bossMesosUniqueAdd;
+		}
+
+		// updates mesos value of boss crystal in html
+		bossesContainer.children[i].children[1].children[1].textContent =
+			type + ": " + bosses[i].mesosChanged.toLocaleString() + " mesos";
+
+		sortAndAdd();
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    EVENT LISTENER FOR INPUTS ON RIGHT SIDE
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+for (let i = 0; i < bosses.length; i++) {
+	bossesContainer.children[i].children[1].children[2].children[0].addEventListener(
+		"change",
+		(event) => {
+			const buttonId = document.querySelector(`#${event.target.id}`);
+			const buttonValue = parseInt(buttonId.value);
+			const bossCrystals = bosses[i].crystals;
+			const bossMesos = bosses[i].mesosChanged;
+			const bossMesosUniqueAdd = bosses[i].mesosUniqueAdd;
+			const type = bosses[i].type;
+
+			//pushes crystal values to array if input increments
+			if (buttonValue > bosses[i].previousValue) {
+				if (bosses[i].name === "Julieta") {
+					for (let i = 0; i < 28; i++) {
+						bossCrystals.push(bossMesos + bossMesosUniqueAdd);
+					}
+				} else if (type === "Daily") {
+					for (let i = 0; i < 7; i++) {
+						bossCrystals.push(bossMesos + bossMesosUniqueAdd);
+					}
+				} else if (type === "Weekly") {
+					for (let i = 0; i < 1; i++) {
+						bossCrystals.push(bossMesos + bossMesosUniqueAdd);
+					}
+				} else {
+					for (let i = 0; i < 1; i++) {
+						bossCrystals.push(bossMesos + bossMesosUniqueAdd);
+					}
+				}
+				//removes crystal values from array if input decreases
+			} else {
+				if (bosses[i].name === "Julieta") {
+					bossCrystals.splice(0, 28);
+				} else if (type === "Daily") {
+					bossCrystals.splice(0, 7);
+				} else if (type === "Weekly") {
+					bossCrystals.splice(0, 1);
+				} else {
+					bossCrystals.splice(0, 1);
+				}
+			}
+			bosses[i].previousValue = parseInt(buttonId.value);
+
+			sortAndAdd();
+		}
+	);
+}
+
+function sortAndAdd() {
+	const counts = {};
+	const newBosses = [];
+	const highestPrices = [];
+
+	//pushes values of crystals from all the objects arrays to one array
+	for (let i = 0; i < bosses.length; i++) {
+		newBosses.push(...bosses[i].crystals);
+	}
+
+	//sorts array highest value to lowest value
+	newBosses.sort(function (a, b) {
+		return b - a;
+	});
+
+	//pushes first 180 values from one array to a new array
+	if (newBosses.length <= 180) {
+		highestPrices.push(...newBosses);
+	} else {
+		for (let i = 0; i < 180; i++) {
+			highestPrices.push(newBosses[i]);
+		}
+	}
+
+	//function for counting specific values in array
+	function count() {
+		for (const num of highestPrices) {
+			counts[num] = counts[num] ? counts[num] + 1 : 1;
+		}
+	}
+	count();
+
+	//changes boss crystal image and amount html based on specific values in array
+	for (let i = 0; i < bosses.length; i++) {
+		const textPath = bossesContainer.children[i].children[2].children[1].children[0];
+		const imgPath = bossesContainer.children[i].children[2].children[0];
+
+		if (counts[bosses[i].mesosChanged + bosses[i].mesosUniqueAdd] === undefined) {
+			textPath.textContent = "x 0";
+			imgPath.innerHTML =
+				"<img src='images/grey.png' alt='' class='bossCrystal' />";
+		} else {
+			textPath.textContent =
+				"x " + counts[bosses[i].mesosChanged + bosses[i].mesosUniqueAdd];
+			imgPath.innerHTML =
+				"<img src='images/blue.png' alt='' class='bossCrystal' />";
+		}
+	}
+	storeInfo();
+	console.log(highestPrices);
+}
+
+if (bossesLs) {
+	for (let i = 0; i < bosses.length; i++) {
+		bossesContainer.children[i].children[1].children[2].children[0].value =
+			bosses[i].previousValue;
+
+		bossesContainer.children[i].children[0].value = bosses[i].previousValueTwo;
+	}
+	sortAndAdd();
+	renderHtml();
+}
+
+function storeInfo() {
+	localStorage.setItem("info", JSON.stringify(bosses));
+}
+
+function reset() {
+	localStorage.clear();
+	for (let i = 0; i < bosses.length; i++) {
+		//input right side html value
+		bossesContainer.children[i].children[1].children[2].children[0].value = 0;
+		//input left side html value
+		bossesContainer.children[i].children[0].value = 1;
+
+		//mesos displayed value
+		bossesContainer.children[i].children[1].children[1].textContent =
+			bosses[i].type +
+			": " +
+			Math.floor(bosses[i].mesos).toLocaleString() +
+			" mesos";
+
+		//boss crystal amount and image reset
+		bossesContainer.children[i].children[2].children[1].children[0].textContent =
+			"x 0";
+		bossesContainer.children[i].children[2].children[0].innerHTML =
+			"<img src='images/grey.png' alt='' class='bossCrystal' />";
+
+		//boss crystals reset
+		bosses[i].crystals = [];
+	}
+}
